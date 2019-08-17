38cd4343fd344f9822bd1aad4c9fb02056933fab
Greyno <gamrey@mat-mac.com>
2019-08-16 21:40:31 -0400

    Initial Commit

---
+++ SwipeFilteredImages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeFilteredImages
+//
+//  Created by Gillian Reynolds-Titko on 8/16/19.
+//  Copyright © 2019 GRey-T-Programs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
